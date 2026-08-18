2017fbbc820bf9075fb5767594cbab256490c4c9
uandwe12-oss <uandwe12@gmail.com>
2026-08-18 20:24:08 +0530

    Update autoCancelAssetRelease.js

--- services/autoCancelAssetRelease.js
+++ services/autoCancelAssetRelease.js
@@ -1,7 +1,6 @@
 const cron = require('node-cron');
 const getDriver = require('../lib/neo4j');
 
-
 function startAutoCancelAssetReleaseScheduler() {
   // Run daily at midnight
   cron.schedule('0 0 * * *', async () => {
