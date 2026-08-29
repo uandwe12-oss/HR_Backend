ea8872a0ab7c62ccd643e0d4288b09acfc3ad8a5
uandwe12-oss <uandwe12@gmail.com>
2026-08-29 22:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const { verifyToken } = require('./middleware/auth');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 async function initializeServices() {
   console.log('\n🔧 Initializing services...');
   await initAutoExport();
