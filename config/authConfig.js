efe6c586744d8ee47ba86f636917496d5566c507
uandwe12-oss <uandwe12@gmail.com>
2026-08-29 22:04:53 +0530

    Add authentication configuration for user roles

---
+++ config/authConfig.js
@@ -0,0 +1,11 @@
+const AUTH_CONFIG = {
+  'Employee': 'SSO_ONLY',
+  'HR': 'SSO_ONLY',
+  'Recruiter': 'BOTH',
+  'Interviewer': 'PASSWORD',
+  'Client Interviewer': 'PASSWORD',
+  'UANDWE Interviewer': 'PASSWORD',
+  'Admin': 'BOTH'
+};
+
+module.exports = AUTH_CONFIG;
